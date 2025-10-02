733097ad0de33f5db4ae2d9ce0451bfb44526581
Mark <mark.deaconu@gmail.com>
2025-10-02 18:22:23 -0400

    added previous problem not in the folder

---
+++ a1q0.r
@@ -0,0 +1,67 @@
+
+bin_distribution <- dbinom(x = c(0, 1), 1, 0.5)
+
+samples <- sample(x = c(0, 1), 100, replace = TRUE, prob = bin_distribution)
+
+matrix_of_samples <- matrix(samples, 10, 10)
+
+row1and7 <- append(matrix_of_samples[1, ], matrix_of_samples[3, ])
+
+row1and3and7 <- append(row1and7, matrix_of_samples[7, ])
+
+frequency_table <- table(row1and3and7)
+
+count_of_1s <- frequency_table["1"]
+
+inside_range <- 16 <= count_of_1s & count_of_1s < 20
+
+percolumn1s <- apply(matrix_of_samples, 2, function(x) sum(x == 0))
+
+
+# b)
+
+x1 <- c(rep(1, 20))
+
+x2 <- c(append(seq(from = 2, to = 20, by = 2), seq(from = 20, to = 2, by = -2)))
+
+x3 <- c(seq(from = 1, to = 20, by = 1))
+
+x <- cbind(x1, x2, x3)
+
+xtx <- t(x) %*% x
+
+invxtx <- solve(xtx)
+
+# checksymmetric <- function(mat){
+#     # issue with zerocount being NA
+#     zerocount <- table(t(mat) %*% mat)["0"]
+#     print(zerocount)
+#     if(zerocount == (nrow(mat) * ncol(mat))){
+#         return(TRUE)
+#     }
+#     else{
+#         return(FALSE)
+#     }
+# }
+
+# print(matrix_of_samples)
+# print(frequency_table)
+# print(unname(count_of_1s))
+
+# print(inside_range)
+
+# print(percolumn1s)
+
+print(x1)
+
+print(x2)
+
+print(x3)
+
+print(x)
+print(xtx)
+print(invxtx)
+
+print(xtx - t(xtx))
+
+print(invxtx - t(invxtx))
